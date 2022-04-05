dd51c778a27409d9b233bf586cec43f0e948f74e
Seth Cattanach <scattanach1@gmail.com>
2022-04-05 02:11:27 +0000

    v2 with mail working

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import requests
 from requests_html import HTMLSession
 from datetime import datetime
+import smtplib
+from retrieve import access_secret_version
+
 
 current_date = datetime.now()
 
@@ -25,4 +28,18 @@ for event in events:
     if cds_stamp > current_date:
         event_candidates.append(candidate)
     
-print('\n'.join(event_candidates))
+
+from_email = 'scattanach1@gmail.com'
+to_email = 'aleah@umich.edu'
+smtpobj = smtplib.SMTP('smtp.gmail.com', 587)
+message_raw = 'New Bellingcat events found:\n\n' + '\n\n'.join(event_candidates) + '\n\n---------\nBEE-DOO-BEE-DOO hi hope you are having a wonderful day, Bean!'
+subject = 'Bellingcat Web Scraper - New Events Found'
+message = 'Subject: {}\n\n{}'.format(subject, message_raw)
+smtpobj.starttls()
+
+# retrieve API key
+access_secret_version(PROJECT_ID, SECRET_ID, VERSION_ID)
+
+smtpobj.login('scattanach1@gmail.com','')
+smtpobj.sendmail(from_email, to_email, message)
+smtpobj.quit()
